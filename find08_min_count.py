0b54d2b6704ffa9cb1f511e492d95896d74fe963
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-05-01 18:23:52 +0500

    8

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,12 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    a=0
+    b=data[0]
+    while a<len(data):
+        if data[a]<b:
+            b=data[a]
+        a+=1
+    d=data.count(b)
+    return d
+print(find_min_count([9, 8, 3, 8,3, 5]))
